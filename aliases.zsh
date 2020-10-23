9db7560d996d52289ded950eb9b228d2ec96e1d8
Ari Frankel <contact@arifrankel.dev>
2020-10-23 20:05:18 +1000

    Create our zsh aliases

---
+++ aliases.zsh
@@ -0,0 +1,43 @@
+# Shortcuts
+alias reloadshell="source $HOME/.zshrc"
+alias c="clear"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias code="cd $HOME/Code"
+
+# Laravel
+alias art="php artisan"
+alias fresh="php artisan migrate:fresh --seed"
+alias seed="php artisan db:seed"
+
+# PHP
+alias cfresh="rm -rf vendor/ composer.lock && composer i"
+alias php74="docker run -it -w /data -v ${PWD}:/data:delegated --entrypoint php --rm registry.gitlab.com/grahamcampbell/php:7.4"
+alias php8="docker run -it -w /data -v ${PWD}:/data:delegated --entrypoint php --rm registry.gitlab.com/grahamcampbell/php:8.0"
+alias composer="php -d memory_limit=-1 /usr/local/bin/composer"
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+alias watch="npm run watch"
+
+# Docker
+alias docker-composer="docker-compose"
+
+# Git
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force"
+alias nah="git clean -df && git reset --hard"
+alias pop="git stash pop"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+alias wip="commit wip"
